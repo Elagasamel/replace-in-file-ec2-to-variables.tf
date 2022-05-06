4b61af0fc4b1d6babfdfa6c245f30de88d938ccf
Elagas Amel <63187083+Elagasamel@users.noreply.github.com>
2022-05-06 09:01:04 +0100

    Create Deployroute53.js

---
+++ frontend/src/pages/network/Deployroute53.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from "react-bootstrap/Button";
+
+const Destroyroutes53 = () => {
+
+  return (
+
+    <>
+    
+    <div className="mb-2">
+    <Button name="b1" size="lg" variant="primary" active> Deploy  to AWS </Button>{' '}
+    </div>
+       
+  
+
+    </> 
+  );
+
+};
+
+
+export default Destroyroutes53;
