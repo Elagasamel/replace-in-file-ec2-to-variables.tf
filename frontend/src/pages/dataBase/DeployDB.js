5f1d2f96a5e858a8f080a31a0bcf8a4f5d1f1aca
Elagas Amel <63187083+Elagasamel@users.noreply.github.com>
2022-05-06 08:59:56 +0100

    Create DeployDB.js

---
+++ frontend/src/pages/dataBase/DeployDB.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from "react-bootstrap/Button";
+import './DynamoDB.css'
+
+const DeployDB = () => {
+
+  return (
+
+    <>
+    
+    <div className="mb-2">
+    <Button name="b1" size="lg" variant="primary" active> Deploy DB to AWS </Button>{' '}
+    </div>
+       
+  
+
+    </> 
+  );
+
+};
+
+
+export default DeployDB;
