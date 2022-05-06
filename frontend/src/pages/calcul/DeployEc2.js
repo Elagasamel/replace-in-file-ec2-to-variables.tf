4bb6c8871a555ad7195b5cd1aeab49fa4de17e5a
Elagas Amel <63187083+Elagasamel@users.noreply.github.com>
2022-05-06 09:03:12 +0100

    Create DeployEc2.js

---
+++ frontend/src/pages/calcul/DeployEc2.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from "react-bootstrap/Button";
+
+const DeployEc2 = () => {
+
+  return (
+
+    <>
+    
+    <div className="mb-2">
+    <Button name="b1" size="lg" variant="primary" active> Deploy DB to AWS </Button>{' '}
+    </div>
+       
+  
+
+    </> 
+  );
+
+};
+
+
+export default DeployEc2;
