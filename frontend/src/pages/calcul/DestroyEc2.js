fe70a5d9eee897d889f154cfb03b8ffbcc57c4a6
Elagas Amel <63187083+Elagasamel@users.noreply.github.com>
2022-05-06 09:02:49 +0100

    Create DestroyEc2.js

---
+++ frontend/src/pages/calcul/DestroyEc2.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from "react-bootstrap/Button";
+
+
+const DestroyEc2 = () => {
+
+
+  
+  return (
+
+    <form>
+    
+    <div className="mb-2">
+    <Button name="b1" size="lg" variant="primary" active> Destroy </Button>{' '}
+    </div>
+
+    </form>
+  );
+
+};
+
+
+export default DestroyEc2;
