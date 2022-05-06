8b7e6f60f59158d78b88a8aee2df2b2d934c814e
Elagas Amel <63187083+Elagasamel@users.noreply.github.com>
2022-05-06 09:00:41 +0100

    Create DestroyDB.js

---
+++ frontend/src/pages/dataBase/DestroyDB.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from "react-bootstrap/Button";
+import './DynamoDB.css'
+
+const DestroyDB = () => {
+
+  return (
+
+    <>
+    
+    <div className="mb-2">
+    <Button name="b1" size="lg" variant="primary" active> Destroy DB </Button>{' '}
+    </div>
+       
+  
+
+    </> 
+  );
+
+};
+
+
+export default DestroyDB;
